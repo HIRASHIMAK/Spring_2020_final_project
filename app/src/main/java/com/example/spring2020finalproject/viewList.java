fbf5d23f20025dc04ab1d9899cb937bb0f53b0e3
mhuang60 <mhuang60@illinois.edu>
2020-04-27 23:08:33 -0500

    added the back button to viewList

--- app/src/main/java/com/example/spring2020finalproject/viewList.java
+++ app/src/main/java/com/example/spring2020finalproject/viewList.java
@@ -1,4 +1,32 @@
 package com.example.spring2020finalproject;
 
-public class viewList {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class viewList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_list);
+
+        configureBackButton();
+    }
+
+    /**
+     * configureBackButton will switch from viewList activity to Main activity
+     */
+    private void configureBackButton() {
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
