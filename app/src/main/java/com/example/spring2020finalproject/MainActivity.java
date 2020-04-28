0bf701c1888888304304e38d11a43fcef5f33c4b
HIRASHIMAK <kentah2@illinois.edu>
2020-04-27 22:19:29 -0500

    Fourth commit

--- app/src/main/java/com/example/spring2020finalproject/MainActivity.java
+++ app/src/main/java/com/example/spring2020finalproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.spring2020finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewListButton = findViewById(R.id.viewListButton);
+        viewListButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), viewList.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/spring2020finalproject/viewList.java
@@ -0,0 +1,4 @@
+package com.example.spring2020finalproject;
+
+public class viewList {
+}
