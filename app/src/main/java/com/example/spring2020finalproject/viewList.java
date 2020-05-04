028493d2a938c07d5cb169461f5ac2ff1fdd314b
HIRASHIMAK <kentah2@illinois.edu>
2020-05-04 13:55:23 -0500

    12th commit

--- app/src/main/java/com/example/spring2020finalproject/viewList.java
+++ app/src/main/java/com/example/spring2020finalproject/viewList.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
